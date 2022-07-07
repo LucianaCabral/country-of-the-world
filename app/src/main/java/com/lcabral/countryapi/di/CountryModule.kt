632358d278699a33388fab39953216799d58b83d
LucianaCabral <eng.luciana.cabral@gmail.com>
2022-07-07 14:35:01 -0300

    Ajustando a tela de detalhes dos países

--- app/src/main/java/com/lcabral/countryapi/di/CountryModule.kt
+++ app/src/main/java/com/lcabral/countryapi/di/CountryModule.kt
@@ -1,12 +1,12 @@
 package com.lcabral.countryapi.di
 
-import com.lcabral.countryapi.source.CountryDataSourceImpl
 import com.lcabral.countryapi.data.CountryService
-import com.lcabral.countryapi.usecase.CountryUseCase
 import com.lcabral.countryapi.repository.CountryDetailsRepository
 import com.lcabral.countryapi.repository.CountryRepository
-import com.lcabral.countryapi.source.CountryDetailsDataSource
+import com.lcabral.countryapi.source.CountryDataSourceImpl
+import com.lcabral.countryapi.source.CountryDetailsDataSourceImpl
 import com.lcabral.countryapi.usecase.CountryDetailsUseCase
+import com.lcabral.countryapi.usecase.CountryUseCase
 import com.lcabral.countryapi.viewmodel.CountryDetailViewModel
 import com.lcabral.countryapi.viewmodel.CountryViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -24,7 +24,7 @@ val countryModule = module {
     factory {
         CountryDetailsUseCase(
             countryDetailsRepository = CountryDetailsRepository(
-                countryService = CountryService()
+                countryDetailsDataSource = CountryDetailsDataSourceImpl(countryService= CountryService())
             )
         )
     }
